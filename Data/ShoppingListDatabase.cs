79895d9cc993bcb48fb119260389a6e558a372c1
UEmanuelT <uifalean_e@yahoo.com>
2022-12-20 20:37:37 +0200

    laborator 10 complet

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -19,6 +19,7 @@ namespace Uifalean_Emanuel_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
         public Task<int> SaveProductAsync(Product product)
         {
@@ -47,8 +48,24 @@ namespace Uifalean_Emanuel_Lab7.Data
         {
             return _database.QueryAsync<ListProduct>("select * from ListProduct");
         }
-                    
-public Task<List<ShopList>> GetShopListsAsync()
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
+
+
+        public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
         }
@@ -68,6 +85,10 @@ public Task<List<ShopList>> GetShopListsAsync()
             {
                 return _database.InsertAsync(slist);
             }
+        }
+        public Task<int> DeleteShopAsync(Shop shop)
+        {
+            return _database.DeleteAsync(shop);
         }
         public Task<int> DeleteShopListAsync(ShopList slist)
         {
